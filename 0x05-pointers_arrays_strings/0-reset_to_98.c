ea01ddf1328336ef657e44bd7ec5115cdf5c5c88
Noubiyoyo <geoffroyolougo@gmail.com>
2022-06-13 08:49:39 +0100

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+# inclure  " main.h "
+/* *
+* reset_to_98 - mise à jour et valeur entière à 98
+* @n : Un pointeur vers un int qui sera mis à jour
+* Retour : 0
+ */
+void  reset_to_98 ( int *n)
+{
+*n = 98 ;
+}
