3ed91c0245cb0c1fcc7f0981d0c4e0eebe9ab45d
Noubiyoyo <geoffroyolougo@gmail.com>
2022-06-15 20:39:04 +0100

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,6 +14,7 @@ char *_strncat(char *dest, char *src)
 int x, y;
 
 for (x = 0; dest[x] != '\0'; x++)
+{
 ;
 for (y = 0; src[y] != '\0'; y++)
 {
@@ -21,6 +22,7 @@ dest[x] = src[y];
 x++;
 }
 dest[x] = '\0';
+}
 return (dest);
 }
 
