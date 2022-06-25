c9562467eea6d3f5ad2b2007960c25c4dab0954d
Noubiyoyo <geoffroyolougo@gmail.com>
2022-06-25 20:24:17 +0100

    initial commit

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -8,10 +8,13 @@
 int _isupper(int c)
 {
 
-	if (c >= 'A' && c <= 'Z')
-	return (1);
+int isupper;
 
+    isupper = 0;
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		isupper = 1;
+	}
 
-	else
-	return (0);
+	return (isupper);
 }
