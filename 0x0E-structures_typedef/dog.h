c624d923a85411c05bc41ebbaaa38d1ecff44c5c
Noubiyoyo <geoffroyolougo@gmail.com>
2022-07-04 17:05:39 +0100

    init

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,15 @@
 /**
- *struct dog - a structure type with elements defined
+ * struct dog - a structure type with elements defined
  * @name: pointer to element that hold dog's name
  * @age: element that holds age
  * @owner: pointer to element that holds owner's name
  */
 
-struct dog
+typedef struct dog
 {
     char *name;
     float age;
     char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
